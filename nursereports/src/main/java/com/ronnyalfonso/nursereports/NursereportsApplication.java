08d7c834cb70426a5a12c9f995f42e119394b79c
Ronny Alfonso <roncareer76@yahoo.com>
2018-08-25 14:05:27 -0400

    initial

---
+++ nursereports/src/main/java/com/ronnyalfonso/nursereports/NursereportsApplication.java
@@ -0,0 +1,12 @@
+package com.ronnyalfonso.nursereports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursereportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursereportsApplication.class, args);
+	}
+}
